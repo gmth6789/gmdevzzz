41875411d469554fb64125e4d050919702a75e3a
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2024-06-27 01:38:35 +0700

    Initial commit

---
+++ app/config/environment.d.ts
@@ -0,0 +1,18 @@
+/**
+ * Type declarations for
+ *    import config from 'my-app/config/environment'
+ */
+declare const config: {
+  environment: string;
+  modulePrefix: string;
+  podModulePrefix: string;
+  locationType: 'history' | 'hash' | 'none';
+  rootURL: string;
+  APP: Record<string, unknown>;
+};
+
+declare const liff: {
+  userId: string;
+};
+
+export default config;
